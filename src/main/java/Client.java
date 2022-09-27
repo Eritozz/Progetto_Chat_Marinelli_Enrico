c020f84b5750c459575820c63993a31af43add02
lai2-01 <lai2-01@itismeucci.com>
2022-09-27 10:40:06 +0200

    Second commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,13 @@
+package it.fi.em;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    String nomeServer = "Server";
+    int portaServer = 7727;
+    DataInputStream in;
+    DataOutputStream out;
+
+
+    
+}
